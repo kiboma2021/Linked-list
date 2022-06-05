8de8eeaa5278d3c132856cab0b719d6d1cc3301b
Ben Kiboma <omayioben@gmail.com>
2022-06-05 22:29:25 +0300

    Create class

--- index_2.js
+++ index_2.js
@@ -6,4 +6,29 @@ class node {
 }
 
 const n1 = new node (100);
-console.log(n1); // Try to run node file name
+//console.log(n1); // Try to run node file name
+
+class LinkedList {
+  constructor () {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Insert head
+
+
+  //Insert Tail
+
+
+  //Insert at index
+
+  //Get at index
+
+  //Remove at index
+
+
+  //Clear List
+
+  
+  //Print List
+}
