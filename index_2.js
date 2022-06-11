fe78f33dc1897e77b78760fea9596a05c2d2be0f
Ben Kiboma <omayioben@gmail.com>
2022-06-11 11:39:17 +0300

    Initialize class node and Linked list class

--- index_2.js
+++ index_2.js
@@ -1,37 +1,15 @@
 class Node {
-  constructor(data, next=null) {
-    this.data = data;
-    this.next = next;
+  constructor(cars) {
+    this.cars = cars;
+    this.next = null;
   }
 }
 
-const n1 = new Node (100);
-//console.log(n1); // Try to run Node file name
-
-class LinkedList {
-  constructor () {
+class linkedList {
+  constructor() {
     this.head = null;
     this.size = 0;
   }
-
-  // Insert head
-  insertFirst (data) {
-    this.head = new Node(data, this.head);
-  }
-
-
-  //Insert Tail
-
-
-  //Insert at index
-
-  //Get at index
-
-  //Remove at index
-
-
-  //Clear List
-
-  
-  //Print List
 }
+
+//Add an element at the end of the list
